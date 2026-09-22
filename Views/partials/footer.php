cdc0131ed0fd17dacad642dd1945dd00379aa236
Angelo <angelopineda963@gmail.com>
2026-09-22 19:18:06 +0800

    Clean duplicate uploads and generated artifacts

--- Views/partials/footer.php
+++
@@ -1,6 +0,0 @@
-    </main>
-    <footer class="site-footer">
-        <div class="container">SimplePOS &copy; <?= date('Y') ?> &middot; CodeIgniter 4 learning project</div>
-    </footer>
-</body>
-</html>
